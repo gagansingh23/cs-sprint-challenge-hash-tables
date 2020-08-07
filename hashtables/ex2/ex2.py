f42a19da048e91c8e09665386df06808cf486533
gagansingh23 <gsinghpnjb@gmail.com>
2020-08-07 13:42:43 -0400

    ex1,2,4

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,22 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    tickets_departure = {}
+    for i in tickets:
+        tickets_departure[i.source] = i
+
+    curr_ticket = tickets_departure["NONE"]
+
+    route = []
+
+    while True:
+        route.append(curr_ticket.destination)
+
+        if curr_ticket.destination is "NONE":
+            break
+
+        curr_ticket = tickets_departure[curr_ticket.destination]
+        print(route)
 
     return route
+
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,9 +2,16 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    corr_neg = {}
+    result = []
+    for i in a:
+        corr_neg[i] = i
+        if i != 0 and -i in corr_neg:
+            result.append(abs(i))
+            print(i)
 
     return result
+
 
 
 if __name__ == "__main__":
