243b787b7b4308afe35808fd07bc4d449a920830
gagansingh23 <gsinghpnjb@gmail.com>
2020-08-07 13:41:21 -0400

    submission

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_idc = {}
+
+    for i in range(length):
+        weight_idc[weights[i]] = i 
+
+    for i in range(length):
+        goal = limit - weights[i]
+        if goal in weight_idc:
+            if weight_idc[goal] > i:
+                return (weight_idc[goal], i)
+            else:
+                return (i, weight_idc[goal])
 
     return None
